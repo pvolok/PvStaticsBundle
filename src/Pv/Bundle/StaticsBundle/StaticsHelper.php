0d113fac87b88d57ebe7f1be9d5e4adbf4b1ba07
Pavel Volokitin <pavelvolokitin@gmail.com>
2012-09-06 23:45:11 +0600

    Add statics.helper service

---
+++ src/Pv/Bundle/StaticsBundle/StaticsHelper.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Pv\Bundle\StaticsBundle;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class StaticsHelper
+{
+    private $container;
+    private $debug;
+
+    function __construct(ContainerInterface $container, $debug)
+    {
+        $this->container = $container;
+        $this->debug = $debug;
+    }
+
+    function path($path)
+    {
+        $locale = $this->container->get('request')->getLocale();
+        $path = preg_replace('/\.js$/', ".$locale.js", $path);
+        if ($this->debug) {
+            return "/s/$path";
+        } else {
+            if (!$this->namesMap) {
+                $this->namesMap = include $this->conatiner->getParameter('kernel.root_dir').'/statics_map.php';
+            }
+            return (isset($this->namesMap[$path]) ? $this->namesMap[$path] : '/');
+        }
+    }
+}
