aa071b9e4aa8401977430109ce585b23b01cba79
Pavel Volokitin <pavelvolokitin@gmail.com>
2013-04-10 16:58:24 +0600

    set content-type later

--- src/Pv/StaticsBundle/Controller/DevController.php
+++ src/Pv/StaticsBundle/Controller/DevController.php
@@ -9,13 +9,13 @@ class DevController extends Controller
 {
     function renderAction($path)
     {
-        $ext = pathinfo($path, PATHINFO_EXTENSION);
         $this->getRequest()->setFormat('less', 'text/css');
-        $this->getRequest()->setRequestFormat($ext);
 
         $content = $this->get('statics.manager')
             ->getFileContent($path, $this->getRequest()->getLocale(), true);
         if ($content) {
+            $ext = pathinfo($path, PATHINFO_EXTENSION);
+            $this->getRequest()->setRequestFormat($ext);
             return new Response($content);
         }
         return new Response('File not found.');
