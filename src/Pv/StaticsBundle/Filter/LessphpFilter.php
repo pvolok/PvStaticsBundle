1ad9f9616618ab48d7566837f3f9b76e17ec57d5
Pavel Volokitin <pavelvolokitin@gmail.com>
2013-11-06 15:20:39 +0600

    added data_uri function for less

--- src/Pv/StaticsBundle/Filter/LessphpFilter.php
+++ src/Pv/StaticsBundle/Filter/LessphpFilter.php
@@ -3,14 +3,40 @@
 namespace Pv\StaticsBundle\Filter;
 
 use Pv\StaticsBundle\Asset\FileAsset;
+use Pv\StaticsBundle\StaticsManager;
 
 class LessphpFilter
 {
+    /**
+     * @var \Pv\StaticsBundle\StaticsManager
+     */
+    private $manager;
+
+    public function __construct(StaticsManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
     public function filter(FileAsset $asset)
     {
         $content = $asset->getContent();
 
         $less = new \lessc();
+
+        $less->registerFunction('data_uri', function($arg) use($asset) {
+            if ($arg[0] !== 'string') {
+                throw new \Exception("The data_uri function accepts only strings ($arg[0] given).");
+            }
+
+            $path = $arg[2][0];
+            if (pathinfo($path, PATHINFO_EXTENSION) !== 'png') {
+                throw new \Exception("Only png allowed in data_uri function ($path).");
+            }
+
+            $data = base64_encode($this->manager->load($path, $asset)->getContent());
+
+            return "url('data:image/png;base64,$data')";
+        });
 
         if (!$asset->getParam('debug')) {
             $less->setFormatter('compressed');
